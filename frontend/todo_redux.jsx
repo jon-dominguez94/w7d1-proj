c7e46d40d0e9ad59565ceb64a45329e884a57968
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-12-03 14:26:52 -0800

    Create store in entry file

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
 const App = () => {
   return (
     <h1>Todos App</h1>
   );
 };
+
+const store = configureStore();
+window.store = store;
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("content");
