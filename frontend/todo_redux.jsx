d34a0d40312aaf1ba3fe3418bc56fb5d1bcada61
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-12-03 15:27:59 -0800

    Setup App component

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -2,12 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import { receiveTodos, receiveTodo } from './actions/todo_actions';
-
-const App = () => {
-  return (
-    <h1>Todos App</h1>
-  );
-};
+import App from './components/app';
 
 const store = configureStore();
 window.store = store;
