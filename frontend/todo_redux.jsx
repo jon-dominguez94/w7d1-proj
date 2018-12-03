b98b0ed50d7dbe7aa19308e582657475c063910a
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-12-03 15:39:53 -0800

    Create root component

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import { receiveTodos, receiveTodo } from './actions/todo_actions';
 import App from './components/app';
+import Root from './components/root';
 
 const store = configureStore();
 window.store = store;
@@ -11,5 +12,5 @@ window.receiveTodos = receiveTodos;
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("content");
-  ReactDOM.render(<App/>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
