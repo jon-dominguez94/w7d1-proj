b402f8991581b045009bc51756b24142d9397828
Jon Dominguez <jon.dominguez94@yahoo.com>
2018-12-04 08:36:34 -0800

    Add todolistitem component to todo list

--- frontend/components/todos/todo_list.jsx
+++ frontend/components/todos/todo_list.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import TodoListItem from './todo_list_item';
 
 class TodoList extends React.Component {
   render() {
     const { todos, receiveTodo } = this.props;
-    const todoTitles = todos.map(todo => <li>{todo.title}</li>);
+    const todoTitles = todos.map(todo => (
+        <TodoListItem key={todo.id} todo={todo} receiveTodo={ receiveTodo } />
+      )
+    );
     return (
       <ul>
         {todoTitles}
